0e5477ac20cde4a76dda2aa8bfcd96784260e1f2
Sandeepa Gunathilaka <dsandeepa.gunathilaka@gmail.com>
2026-04-10 20:59:15 +0530

    Initial backend commit

---
+++ backend/src/main/java/com/nexus/scampus/ScampusBakendApplication.java
@@ -0,0 +1,13 @@
+package com.nexus.scampus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScampusBakendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScampusBakendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/nexus/scampus/ScampusBakendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nexus.scampus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScampusBakendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

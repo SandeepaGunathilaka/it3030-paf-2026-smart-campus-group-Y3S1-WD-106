0d5c77b09e053faba3e768083aebca859dfc3065
Sandeepa Gunathilaka <dsandeepa.gunathilaka@gmail.com>
2026-04-30 01:36:53 +0530

    fixed main branch issues

--- backend/src/test/java/com/nexus/scampus/ScampusBakendApplicationTests.java
+++ backend/src/test/java/com/nexus/scampus/ScampusBakendApplicationTests.java
@@ -2,8 +2,10 @@ package com.nexus.scampus;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ScampusBakendApplicationTests {
 
 	@Test
